33bb9e7bc72ef0e23fdb7f93c250849b34bdf04e
crycodex <recaldecd@gmail.com>
2025-11-06 16:11:04 -0500

    Add MovieItem widget to enhance movie display in search results, featuring poster, title, overview, and rating for improved user experience.

--- cine_app/lib/presentation/delegates/search_movie_delegate.dart
+++ cine_app/lib/presentation/delegates/search_movie_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cine_app/domain/entities/movie.dart';
+import 'package:cine_app/config/helpers/numformat.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
@@ -45,10 +46,70 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemCount: movies.length,
           itemBuilder: (context, index) {
             final movie = movies[index];
-            return ListTile(title: Text(movie.title));
+            return MovieItem(movie: movie);
           },
         );
       },
+    );
+  }
+}
+
+class MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const MovieItem({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: [
+          /* poster */
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image(
+              image: NetworkImage(movie.posterPath),
+              width: size.width * 0.2,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 10),
+          /* title and overview */
+          SizedBox(
+            width: (size.width - 80) * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* title */
+                Text(movie.title, style: textStyles.titleMedium),
+                /* overview */
+                (movie.overview.length > 100)
+                    ? Text("${movie.overview.substring(0, 100)}...")
+                    : Text(movie.overview),
+                /* rating */
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.yellow.shade700,
+                    ),
+                    Text(
+                      Numformat.number(movie.popularity),
+                      style: textStyles.bodySmall?.copyWith(
+                        color: Colors.yellow.shade700,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
