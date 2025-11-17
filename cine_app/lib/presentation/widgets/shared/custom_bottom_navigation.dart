07671d720471aab4d606a278d363fa69cda6c4e7
crycodex <recaldecd@gmail.com>
2025-11-17 10:15:50 -0500

    Implement custom bottom navigation with dynamic index and tap handling using GoRouter for improved navigation experience.

--- cine_app/lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ cine_app/lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
+
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case "/":
+        return 0;
+      case "/categories":
+        return 1;
+      case "/favorites":
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTap(BuildContext context, int value) {
+    switch (value) {
+      case 0:
+        context.go("/");
+        break;
+      case 1:
+        context.go("/categories");
+        break;
+      case 2:
+        context.go("/favorites");
+        break;
+      default:
+        context.go("/");
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
-      items: [
+      currentIndex: getCurrentIndex(context),
+      onTap: (index) => onItemTap(context, index),
+      items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         BottomNavigationBarItem(
-          icon: Icon(Icons.category),
+          icon: Icon(Icons.category_sharp),
           label: "Categories",
         ),
         BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorites"),
