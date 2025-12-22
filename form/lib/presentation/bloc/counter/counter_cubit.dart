cdb9c87a0db57961e0c628cf9d11e924b8c5fab1
Cristhian David Recalde <recaldecd@gmail.com>
2025-12-22 17:26:13 -0500

    Enhance CounterCubit and CubitCounterScreen with reset and refresh functionalities, and improve UI to display transaction count. Refactor state management for better performance.

--- form/lib/presentation/bloc/counter/counter_cubit.dart
+++ form/lib/presentation/bloc/counter/counter_cubit.dart
@@ -6,14 +6,37 @@ class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counter: 0, transactions: 0));
 
   void increment1() {
-    emit(CounterState(counter: state.counter + 1, transactions: state.transactions + 1));
+    emit(
+      CounterState(
+        counter: state.counter + 1,
+        transactions: state.transactions + 1,
+      ),
+    );
   }
 
   void increment10() {
-    emit(CounterState(counter: state.counter + 10, transactions: state.transactions + 1));
+    emit(
+      CounterState(
+        counter: state.counter + 10,
+        transactions: state.transactions + 1,
+      ),
+    );
   }
 
   void decrement10() {
-    emit(CounterState(counter: state.counter - 10, transactions: state.transactions + 1));
+    emit(
+      CounterState(
+        counter: state.counter - 10,
+        transactions: state.transactions + 1,
+      ),
+    );
+  }
+
+  void reset() {
+    emit(CounterState(counter: 0, transactions: state.transactions + 1));
+  }
+
+  void refresh() {
+    emit(CounterState(counter: 0, transactions: 0));
   }
 }
--- form/lib/presentation/screens/cubit_counter_screen.dart
+++ form/lib/presentation/screens/cubit_counter_screen.dart
@@ -1,39 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:form/presentation/bloc/counter/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: _CubitCounterView(),
+    );
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final countState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Counter"),
+        title: Text("Counter Transacciones: ${countState.transactions}"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.refresh_rounded)),
+          IconButton(
+            onPressed: () {
+              context.read<CounterCubit>().refresh();
+            },
+            icon: Icon(Icons.refresh_rounded),
+          ),
         ],
       ),
-      body: Center(child: Text("Contador")),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          buildWhen: (previous, current) => current.counter != previous.counter,
+          builder: (context, state) {
+            return Text("Contador: ${state.counter}");
+          },
+        ),
+      ),
       //floating action button
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SizedBox(width: 50),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<CounterCubit>().increment1();
+            },
             heroTag: "1",
             child: Text("+1"),
           ),
           SizedBox(width: 10),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<CounterCubit>().increment10();
+            },
             heroTag: "10",
             child: Text("+10"),
           ),
           SizedBox(width: 10),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<CounterCubit>().decrement10();
+            },
             heroTag: "-10",
             child: Text("-10"),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterCubit>().reset();
+            },
+            child: Icon(Icons.refresh_rounded),
           ),
         ],
       ),
