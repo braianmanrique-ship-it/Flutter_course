ccc51bb06c0d1a2665dc10161c1e0f262ed8fb4b
Cristhian David Recalde <recaldecd@gmail.com>
2026-02-23 08:57:05 -0500

    Add ImageNotification target to Podfile and include Firebase/Messaging dependency. Update project.pbxproj to reference new framework and embed extension for improved notification handling.

---
+++ notifications/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Cristhian  Recalde  on 23/2/26.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
